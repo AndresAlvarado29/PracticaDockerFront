cdaf38c6b3a56dcb4ce20a3459c2b5815c6c959a
Andres Alvarado <nalvaradoc1@est.ups.edu.ec>
2024-01-14 23:48:40 -0500

    cambios en la url de los servicios del front

--- src/app/servicios/servicio.service.ts
+++ src/app/servicios/servicio.service.ts
@@ -10,43 +10,46 @@ import { Curso } from '../domain/curso';
 export class ServicioService {
 
   constructor(private http: HttpClient) { }
+
+  //link para acceder a los microservicios con docker
+  //http://localhost:8080/MicroserviciosRest/api/cliente/crear
   save(cliente: Cliente){
-    return this.http.post<any>("http://localhost:8080/MicroserviciosRest/api/cliente/crear", cliente)
+    return this.http.post<any>("http://34.122.155.171/MicroserviciosRest/api/cliente/crear", cliente)
   }
   buscarEstudiante(cedula: String){
-    return this.http.get<any>("http://localhost:8080/MicroserviciosRest/api/cliente/buscar/"+ cedula)
+    return this.http.get<any>("http://34.122.155.171/MicroserviciosRest/api/cliente/buscar/"+ cedula)
     }
     actualizar(cliente: Cliente){
-    return this.http.post<any>("http://localhost:8080/MicroserviciosRest/api/cliente/crear", cliente)
+    return this.http.post<any>("http://34.122.155.171/MicroserviciosRest/api/cliente/crear", cliente)
     }
   getAll(){
-    return this.http.get<any>("http://localhost:8080/MicroserviciosRest/api/cliente/all")
+    return this.http.get<any>("http://34.122.155.171/MicroserviciosRest/api/cliente/all")
     }
   delete(cedula: string) {
-    return this.http.delete<any>("http://localhost:8080/MicroserviciosRest/api/cliente/borrar/"+cedula);
+    return this.http.delete<any>("http://34.122.155.171/MicroserviciosRest/api/cliente/borrar/"+cedula);
   }
   saveProfesor(profesor: Profesor){
-    return this.http.post<any>("http://localhost:8080/MicroserviciosRest/api/docente/crear", profesor)
+    return this.http.post<any>("http://34.122.155.171/MicroserviciosRest/api/docente/crear", profesor)
     }
   getAllProfesores(){
-    return this.http.get<any>("http://localhost:8080/MicroserviciosRest/api/docente/all")
+    return this.http.get<any>("http://34.122.155.171/MicroserviciosRest/api/docente/all")
     }
   buscar(cedula: String){
-    return this.http.get<any>("http://localhost:8080/MicroserviciosRest/api/docente/buscar/"+ cedula)
+    return this.http.get<any>("http://34.122.155.171/MicroserviciosRest/api/docente/buscar/"+ cedula)
     }
     deleteD(cedula: string) {
-      return this.http.delete<any>("http://localhost:8080/MicroserviciosRest/api/docente/borrar/"+cedula);
+      return this.http.delete<any>("http://34.122.155.171/MicroserviciosRest/api/docente/borrar/"+cedula);
     }  
   saveCurso(curso: Curso){
-    return this.http.post<any>("http://localhost:8080/MicroserviciosRest/api/curso/guardar", curso)
+    return this.http.post<any>("http://34.122.155.171/MicroserviciosRest/api/curso/guardar", curso)
     }
   getAllCursos(){
-    return this.http.get<any>("http://localhost:8080/MicroserviciosRest/api/curso/all")
+    return this.http.get<any>("http://34.122.155.171/MicroserviciosRest/api/curso/all")
     }
     deleteC(id: number) {
-      return this.http.delete<any>("http://localhost:8080/MicroserviciosRest/api/curso/borrar/"+id);
+      return this.http.delete<any>("http://34.122.155.171/MicroserviciosRest/api/curso/borrar/"+id);
     } 
   buscarProfesor(cedula: String){
-    return this.http.get<any>("http://localhost:8080/MicroserviciosRest/api/docente/buscar"+cedula)
+    return this.http.get<any>("http://34.122.155.171/MicroserviciosRest/api/docente/buscar"+cedula)
     }     
 }
